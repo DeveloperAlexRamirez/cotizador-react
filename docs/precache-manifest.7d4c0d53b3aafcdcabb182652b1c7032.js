bd727c8409af473210615be4f76cc6591bd7ac93
Alejandro Ramirez <alexkey0516@gmail.com>
2020-09-16 18:53:24 -0500

    Second commit

---
+++ docs/precache-manifest.7d4c0d53b3aafcdcabb182652b1c7032.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9796b9208fab89ac076d732516c3c5a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "78dee643340bc96f1f24",
+    "url": "/static/css/main.ffe5732d.chunk.css"
+  },
+  {
+    "revision": "e38a14487b783e33a0a0",
+    "url": "/static/js/2.86e65ed7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.86e65ed7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "78dee643340bc96f1f24",
+    "url": "/static/js/main.daa1124d.chunk.js"
+  },
+  {
+    "revision": "67e45de583dadb924782",
+    "url": "/static/js/runtime-main.620d9de5.js"
+  }
+]);
